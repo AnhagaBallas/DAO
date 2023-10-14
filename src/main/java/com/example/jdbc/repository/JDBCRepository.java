7f1cff2d63321a1aa7ff235d01714923c239451b
AnhagaBallas <102907803+AnhagaBallas@users.noreply.github.com>
2023-10-14 19:47:14 +0300

    Update JDBCRepository.java

--- src/main/java/com/example/jdbc/repository/JDBCRepository.java
+++ src/main/java/com/example/jdbc/repository/JDBCRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 public interface JDBCRepository {
 
     List<String> getProductName(String name);
+    
     void addCustomer(int age,String name,String surname,String phone,String amount,String date,String productName);
 
 }
